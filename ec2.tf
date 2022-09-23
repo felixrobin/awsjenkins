4146ed808322d97986d9b7d2b642720d160a63e9
felixrobin <111524695+felixrobin@users.noreply.github.com>
2022-09-23 22:02:00 +0100

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "myproject-ec2" {
     ami = "ami-00785f4835c6acf64"
     instance_type = "t2.micro"
     tags = {
-    bu = "aws-via-jenkins1"
+    bu = "aws-via-jenkins10"
   }
 }
 
@@ -10,6 +10,6 @@ resource "aws_instance" "myproject-second-ec2" {
     ami = "ami-00785f4835c6acf64"
     instance_type = "t2.micro"
     tags = {
-    bu = "aws-via-jenkins2"
+    bu = "aws-via-jenkins20"
   }
 }
